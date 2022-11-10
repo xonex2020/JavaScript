10a47ca5fb30466d01640e43e22c19bc5a676e90
Dominik <dominik.fh@web.de>
2022-11-10 12:22:06 +0100

    Praxis-Projekt: Teil 3

--- liqui-planner/src/js/main.js
+++ liqui-planner/src/js/main.js
@@ -1,29 +1,59 @@
 "use strict";
 
+let einnahmen = 0;
+let ausgaben = 0;
+let bilanz = 0;
+
 // Eingabedaten erfassen
 let titel_1 = prompt("Titel");
-console.log(`Titel: ${titel_1}`);
-let typ_1 = prompt("Einnahme/Augabe");
-console.log(`Typ: ${typ_1}`);
+let typ_1 = prompt("Einnahme/Ausgabe");
 let betrag_1 = parseInt(prompt("Betrag")); 
-console.log(`Betrag: ${betrag_1} ct`);
-let datum_ = prompt("Datum");
-console.log(`Datum: ${datum_1}`);
+let datum_1 = prompt("Datum");
+console.log(`Titel: ${titel_1}
+Typ: ${typ_1}
+Betrag: ${betrag_1} ct
+Datum: ${datum_1}`
+);
+
+if (typ_1 === "Einnahme") {
+    console.log()
+    einnahmen = einnahmen + betrag_1;
+    bilanz = bilanz + betrag_1;
+} else if (typ_1 === "Ausgabe") {
+    ausgaben = ausgaben - betrag_1;
+    bilanz = bilanz - betrag_1;
+} else {
+    console.log(`Der Typ "${typ_1}" ist nicht bekannt!`);
+}
+
 
 let titel_2 = prompt("Titel");
-console.log(`Titel: ${titel_2}`);
-let typ_2 = prompt("Einnahme/Augabe");
-console.log(`Typ: ${typ_2}`);
-let betrag_2 = parseint(prompt("Betrag")); 
-console.log(`Betrag: ${betrag_2} ct`);
+let typ_2 = prompt("Einnahme/Ausgabe");
+let betrag_2 = parseInt(prompt("Betrag")); 
 let datum_2 = prompt("Datum");
-console.log(`Datum: ${datum_2}`);
+console.log(`Titel: ${titel_2}
+Typ: ${typ_2}
+Betrag: ${betrag_2} ct
+Datum: ${datum_2}`
+);
+
+
+if (typ_2 === "Einnahme") {
+    einnahmen = einnahmen + betrag_2;
+    bilanz = bilanz + betrag_2;
+} else if (typ_2 === "Ausgabe") {
+    ausgaben = ausgaben - betrag_2;
+    bilanz = bilanz - betrag_2;
+} else {
+    console.log(`Der Typ "${typ_2}" ist nicht bekannt!`);
+}
+
 
 // Gesamtbilanz
-let einnahmen;
-let ausgaben
-let bilanz = betrag_1 + betrag_2;
-console.log(`Bilanz: ${bilanz}`);
 
-let positiv = bilanz >= 0;
-console.log(`Bilanz ist positiv: ${positiv}`);
+let bilanzStatus = bilanz >= 0;
+console.log(`Einnahmen: ${einnahmen}
+Ausgaben: ${ausgaben}
+Bilanz: ${bilanz}
+Bilanz Status: ${positiv}`
+);
